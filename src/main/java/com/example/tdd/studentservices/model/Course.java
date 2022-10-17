759882df01d0d569b4f856b2492526bf3ce651a4
Isha Singh <isha080400@gmail.com>
2022-10-17 13:51:51 +0530

    Added the data members to the entities

--- src/main/java/com/example/tdd/studentservices/model/Course.java
+++ src/main/java/com/example/tdd/studentservices/model/Course.java
@@ -1,4 +1,71 @@
 package com.example.tdd.studentservices.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.List;
+
+@Entity
 public class Course {
+
+    @Id
+    private String id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "steps")
+    private List<String> steps;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<String> steps) {
+        this.steps = steps;
+    }
+
+    public Course(){
+    }
+
+    public Course(String id, String name, String description, List<String> steps) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.steps = steps;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", steps=" + steps +
+                '}';
+    }
 }
--- src/main/java/com/example/tdd/studentservices/model/Student.java
+++ src/main/java/com/example/tdd/studentservices/model/Student.java
@@ -1,4 +1,45 @@
 package com.example.tdd.studentservices.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Student {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+
+    public Student(){}
+
+    public Student(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
